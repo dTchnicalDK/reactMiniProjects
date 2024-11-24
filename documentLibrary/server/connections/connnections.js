0a0fbcf11e8011a86cd1274c88430bd0ec14d769
dtechnicaldk <dtechnicaldk@gmail.com>
2024-11-24 11:33:17 +0530

    now server started with proper folder structure like router and controller with mongodb atlas connection and working properly

--- documentLibrary/server/connections/connnections.js
+++ documentLibrary/server/connections/connnections.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 async function connnections(app, port, dbURI) {
   try {
-    //stargin server
+    //starting server
     await app.listen(port, () => {
       console.log(`server started on http://localhost:${port}`);
     });
@@ -17,7 +17,7 @@ async function connnections(app, port, dbURI) {
         console.log(`database connection error : ${err}`);
       });
   } catch (error) {
-    console.log(`server or database connection error:- ${error}`);
+    console.log(`server connection error:- ${error}`);
   }
 }
 
